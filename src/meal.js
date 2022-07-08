62d184d5d5088ee66a7a17d178dea397304d8686
Jenn Yacoubian <jyacoubian@gmail.com>
2022-07-08 17:20:30 -0600

    Complete meal test

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,51 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  var menuItemName = name
+  return `Delicious ${menuItemName}`
 }
 
+function createMenuItem(name, price, type) {
+  var item = {
+    name: name,
+    price: price,
+    type: type,
+  }
+  return item
+}
+
+function addIngredients (toppings, array) {
+  if(array.includes(toppings)) {
+  array
+} else {
+  array.push(toppings)
+}
+  return array
+}
+
+function formatPrice(cost) {
+  return `$${cost}`
+}
+
+function decreasePrice(cost) {
+  newCost = cost*0.9
+  return newCost
+}
+
+function createRecipe(title, ingredients, type) {
+  var recipe = {
+    title: title,
+    ingredients: ingredients,
+    type: type
+  }
+
+  return recipe
+
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
